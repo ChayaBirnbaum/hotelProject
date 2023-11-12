66577efc33d22bf89e958a26ac324a8a9b4eea80
ChayaBirnbaum <cb036767236@gmail.com>
2023-11-12 17:59:56 +0200

    after pull

---
+++ hotelProject/hotelProject/Entities/DataContext.cs
@@ -0,0 +1,20 @@
+﻿namespace hotelProject.Entities
+{
+    public class DataContext
+    {
+        public List<Customers> customersList { get; set; }
+        public List<Rooms> roomsList { get; set; }
+        public List<Orders> ordersList { get; set; }
+        public  int[] roomNumber;
+        public  int orederNum { get; set; }
+
+        public DataContext()
+        {
+            customersList = new List<Customers>();
+            roomsList = new List<Rooms>();
+            ordersList = new List<Orders>();
+            roomNumber = new int[8];
+            orederNum = 1;
+        }
+    }
+}
